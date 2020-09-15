05e4186483d188d2b331f98f184d8b59d036175e
Sadat <sadat0akhtar@gmail.com>
2020-09-15 13:56:31 +0100

    not completed yet, just got to total up the items in the object

--- week-3/Homework/mandatory/2-exercises/exercise-3/exercise-3.js
+++ week-3/Homework/mandatory/2-exercises/exercise-3/exercise-3.js
@@ -1,9 +1,28 @@
  let order = [
-     { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29},
-     { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39},
-     { itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.80},
+     { itemName: "Hot cakes       ", quantity: 1, unitPrice: 2.29},
+     { itemName: "Apple Pie       ", quantity: 2, unitPrice: 1.39},
+     { itemName: "Egg McMuffin    ", quantity: 1, unitPrice: 2.80},
      { itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.00},
-     { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.00},
-     { itemName: "Hash Brown", quantity: 4, unitPrice: 0.40}
+     { itemName: "Hot Coffee      ", quantity: 2, unitPrice: 1.00},
+     { itemName: "Hash Brown      ", quantity: 4, unitPrice: 0.40}
  ]
- 
+ let grandTotal;
+ console.log("QTY     ITEM                 TOTAL");
+
+ order.filter(items => {
+     let {itemName, quantity, unitPrice} = items;
+     totalItemsCost = getTotal(quantity, unitPrice);
+     finalTotal = totalSum(totalItemsCost);
+     console.log(`${quantity}       ${itemName}     ${totalItemsCost}`);      
+ })
+
+//CALCULATES PRICE FOR ITEMS BASED ON QUANTITY
+function getTotal(quantity, price){
+    return quantity * price;
+}
+
+function totalSum(price){
+   return price += totalItemsCost;
+}
+
+console.log(`Total: ${finalTotal}`);
